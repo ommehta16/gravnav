03a3cb0e0da20321103efe58ea545a51a700ed66
Om Mehta <ommehta16@icloud.com>
2026-01-28 01:05:29 -0500

    hopefully the do server works 🙏

--- src/getdata.js
+++ src/getdata.js
@@ -8,7 +8,8 @@
  */
 
 
-const OSM_API_URL = "http://127.0.0.1:3000" // "http://104.236.249.234:3000";
+const OSM_API_URL = //"http://127.0.0.1:3000";
+"http://104.236.249.234:3000";
 
 /**
  * @param {string} query
