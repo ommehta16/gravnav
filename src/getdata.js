b1c00fbe29dfdfd1d7ec5100802aad70b8fc0d52
Om Mehta <ommehta16@icloud.com>
2026-01-30 11:47:58 -0500

    ok maybe

--- src/getdata.js
+++ src/getdata.js
@@ -8,7 +8,7 @@
  */
 
 
-const OSM_API_URL = "https://api.gravnav.ommehta16.us";//"http://127.0.0.1:3000";
+const OSM_API_URL = "http://api.gravnav.ommehta16.us";//"http://127.0.0.1:3000";
 //"https://104.236.249.234:3000";
 
 /**
